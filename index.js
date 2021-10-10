1fbd5d348a15b34f8676e0b4e607f749facf683a
Gabriella Delgado <m.gabriella.delgado@gmail.com>
2021-10-09 21:01:35 -0400

    commit tree 09d03256f5d974e61a1859d56f3fe0073cde2484

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+const wrapAdjective = function(flair="*") {
+    return function(adjective="special") {
+      return `You are ${flair}${adjective}${flair}!`
+    }
+  }
